b1a5a890b4977a35e16e75a0d41d49d829a16a13
unknown <gabriel_picaz@estudante.sc.senai.br>
2020-04-14 16:21:30 -0300

    videos projetos

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <caixas :caixinhas="caixinhas"/>
+    <ferramentas :caixinhas="caixinhas" @alteraCor="alteraCor($event)"/>
+  </div>
+</template>
+
+<script>
+import caixas from "./caixas.vue"
+import ferramentas from "./ferramentas.vue"
+
+export default {
+  name: 'App',
+  components: {
+    caixas,ferramentas
+  }, data:function(){
+    return{
+      caixinhas:[{
+        titulo: "1",
+        cor: "background: yellow"
+      },{
+        titulo: "2",
+        cor: "background: green"
+      },{
+        titulo: "3",
+        cor: "background: red"
+      }]
+    }
+  }, methods:{
+    alteraCor: function(event){
+      this.caixinhas[event.caixa].cor=event.cor      
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/ferramentas.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="ferramentas">
+    <select v-model="caixaSel">
+        <option value="" selected disabled>Selecione uma caixa</option>
+        <option v-for="(caixa, index) in caixinhas" :key="index" :value="index">
+            {{ caixa.titulo }}
+            </option>
+    </select>
+
+    <div id="paleta">
+        <div class="cor" :style="cor" v-for="(cor,index) in cores" :key="index" @click="alteraCor(cor)">
+
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ferramentas",
+    props: ['caixinhas'],
+    data: function() {
+        return{
+            cores: ['background: orange','background: blue','background: magenta'],
+            caixaSel:''
+        }
+    }, methods: {
+        alteraCor: function(cor){
+            this.$emit('alteraCor', {
+                cor: cor,
+                caixa: this.caixaSel
+            })
+        }
+    }
+}
+</script>
+
+<style>
+    #ferramentas{
+        width: 380px;
+        height: 230px;
+        border: 1px solid #fff;
+        margin: 5px auto 0px auto;
+        padding: 10px;
+    }
+    #ferramentas select{
+        width: 380px;
+    }
+    #paleta{
+        width: 380px;
+        height: 230px;
+        display: flex;
+    }
+    .cor{
+        width: 60px;
+        height: 60px;
+        border: 1px solid #fff;
+        margin: 5px 3px 0 0;
+        cursor: pointer;
+    }
+</style>
